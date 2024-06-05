56cbfffc5ff59cf880283100b89b2e01dfaff536
sebasgomez01 <sebasgomezp01@gmail.com>
2024-06-05 19:18:23 -0300

    commit inicial

---
+++ cardatabase/src/test/java/com/csgp/cardatabase/CardatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csgp.cardatabase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
