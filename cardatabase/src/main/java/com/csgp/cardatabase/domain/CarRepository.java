ae6f7531b9a54d7b5c04c44c61ad336e0e67fc58
sebasgomez01 <85655850+sebasgomez01@users.noreply.github.com>
2024-06-06 04:04:57 +0000

    agrego algunas querys a CarRepository para encontrar autos por marca, color, marca y color, etc.

--- cardatabase/src/main/java/com/csgp/cardatabase/domain/CarRepository.java
+++ cardatabase/src/main/java/com/csgp/cardatabase/domain/CarRepository.java
@@ -1,7 +1,27 @@
 package com.csgp.cardatabase.domain;
 
 import org.springframework.data.repository.CrudRepository;
+import java.util.List;
+
 
 public interface CarRepository extends CrudRepository<Car, Long> {
-    
+
+    // obtener todos los autos de una marca determinada
+    List<Car> findByBrand(String brand);
+
+     // obtener todos los autos de una marca determinada ordenados por el año del modelo en forma asc
+     List<Car> findByBrandOrderByModelYearAsc(String brand);
+
+    // obtener todos los autos de un determinado color
+    List<Car> findByColor(String color);
+
+    // obtener todos los autos de un año determinado
+    List<Car> findByModelYear(int modelYear);
+
+    // obtener todos los autos de una marca y color determinado
+    List<Car> findByBrandAndColor(String brand, String color);
+
+    // obtener todos los autods de una marca y modelo determinado
+    List<Car> findByBrandAndModel(String brand, String model);
+
 }
